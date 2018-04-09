7fa6e5ff55c9617ac154087921db525de585e271
vishnuh655 <vishnuh655@gmail.com>
2018-04-09 20:51:35 +0530

    Create order model

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const Order = require('../models/order')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
